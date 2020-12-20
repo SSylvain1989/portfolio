81ae6fdcea69db1a590b9a720b6193ddd6cc82c9
SSylvain1989 <sylvain.sigonnez@gmail.com>
2020-12-20 22:49:55 +0100

    new css project

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,6 +1,7 @@
 // == Import npm
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Icon } from 'semantic-ui-react';
 
 // == Import
 
@@ -8,18 +9,20 @@ import './styles.scss';
 
 // == Composant
 const Project = ({
-  fullName, stack, description, github, image,
+  fullName, stack, description, github, image, website,
 }) => (
   <div className="card">
   <div className="face1">
   <div className="content">
     <img className="picture" src={image} alt="hero" />
+    <h3 className="face1__subtitle">{fullName}</h3>
   </div>
 </div>
 <div className="face2">
   <div className="content">
     <p className="infos"> Stack : {stack}  <br /> Description : {description}</p>
-    <a className="button" href={github}>github</a>
+    <a className="button__one" href={github} label="github"><Icon name="github" size="large" /></a>
+    <a className="button__two" href={website} label="site_web"><Icon name="external alternate" size="large" /></a>
   </div>
 </div>
 </div>
