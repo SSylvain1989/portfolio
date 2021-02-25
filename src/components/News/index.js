f4c07b4a8e70c29a09bb97b3fb9ca438dec003f3
SSylvain1989 <sylvain.sigonnez@gmail.com>
2021-02-25 18:01:02 +0100

    change text

--- src/components/News/index.js
+++ src/components/News/index.js
@@ -5,9 +5,9 @@ import './style.scss';
 const News = () => (
   <div className="News">
     <h2>Aujourd'hui</h2>
-    <p>Après avoir fini ma formation , passé ma certification opquast pour appréhender au mieux les bonnes pratiques du web, j'apprends <a href="https://nextjs.org/">NextJS</a> . Ce framework permettra à mes futurs projets <span>React</span> d'être indexables plus facilement et d'avoir une vitesse d'affichage plus importante grace au rendu côté serveur.</p>
+    <p>Après avoir fini ma formation de <strong>développeur web</strong> Full stack Javascript , passé ma certification <a href="https://www.opquast.com/">opquast</a> pour appréhender au mieux les bonnes pratiques du web, j'apprends <a href="https://nextjs.org/">NextJS</a> . Ce framework permettra à mes futurs applications Web React d'être indexables plus facilement et d'avoir une vitesse d'affichage plus importante grace au rendu côté serveur.</p>
     <p>Mon objectif cette année est de me familiariser aux frameworks <a href="https://vuejs.org/">VueJS</a> et <a href="https://fr.nuxtjs.org/">NuxtJS</a> pour élargir au maximum mes compétences.
-      Je me concentre donc aujourd'hui sur une montée en compétence côté Frontend.
+      Je me concentre donc aujourd'hui sur une montée en compétence côté <strong>Front-end</strong>.
       Néanmoins je reste tout à fait capable de produire un projet complet avec une partie
       Backend en utilisant des outils tel qu' Express avec NodeJS.
     </p>
